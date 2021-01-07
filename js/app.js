33fa366d340568f7509cf0930561f64d67ff5cc6
Danil Kostyn <kostynd1@gmail.com>
2021-01-08 00:39:19 +0200

    animate

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
-new Swiper('.swiper-container-programs', {
-    speed: 400,
-    spaceBetween: 30,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-});
-new Swiper('.swiper-container-examples', {
-    spaceBetween: 30,
-    slidesPerView: 4,
-    slidesPerGroup: 4,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
+(function(){
+    // Sliders
+    new Swiper('.swiper-container-programs', {
+        speed: 400,
+        spaceBetween: 30,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+    new Swiper('.swiper-container-examples', {
+        spaceBetween: 30,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+    
+    // Scroll Animation
+    AOS.init({
+        once: true
+    });
+})();
